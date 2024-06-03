4765f7589abe621d936a99f003fd62dd744a50c2
= <erwinjbofetiado@gmail.com>
2024-06-03 17:14:58 -0500

    que

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,5 +1,5 @@
 type Node<T> = {
-    val: T;
+    value: T;
     next?: Node<T>;
 };
 
@@ -14,10 +14,32 @@ export default class Queue<T> {
     }
 
     enqueue(item: T): void {
-        if (this.length === 0) {
-            this.head = this.tail = new Node();
+        const node = { value: item } as Node<T>;
+
+        if (!this.head) {
+            this.head = this.tail = node;
+        } else if (!this.tail) {
+            this.tail = this.head;
+            this.head = node;
+            this.head.next = this.tail;
+        } else {
+            let tail = this.tail;
+            this.tail = node;
+            tail.next = this.tail;
         }
+
+        this.length++;
     }
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+    deque(): T | undefined {
+        if (!this.head) return undefined;
+
+        const head = this.head;
+        this.head = this.head.next;
+        head.next = undefined;
+        this.length--;
+        return head.value;
+    }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
