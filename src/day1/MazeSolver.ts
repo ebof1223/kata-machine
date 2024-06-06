ed79523cbdab507ff703bbcfe4d997046d729592
ebof1223 <erwinjbofetiado@gmail.com>
2024-06-06 18:49:45 -0500

    maze

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,38 @@
+type Point = {
+    x: number;
+    y: number;
+};
+
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    seen: boolean[][],
+    end: Point,
+): boolean {
+    //base
+
+    //out of bounds
+    if (
+        curr.x < 0 ||
+        curr.x > maze[0][0].length - 1 ||
+        curr.y < 0 ||
+        curr.y > maze[0].length - 1
+    )
+        return false;
+
+    //wall
+    if (maze[curr.y][curr.x] === wall) return false;
+
+    //seen before
+    if (seen[curr.y][curr.x]) return false;
+
+    //reached the end
+    if (curr.y === end.y && curr.x == end.x) return true;
+
+    return true;
+}
+
 export default function solve(
     maze: string[],
     wall: string,
